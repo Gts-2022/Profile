30a4977b6d5e3519f103d72c75486f7e7fd1aac2
Tom <gts-2010@mail.ru>
2023-02-20 16:54:11 +0500

    Initial commit

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    public FormDate birthday; // дата рождения
+    public String name; // поле для имени
+    public String passport; // поле для номера и серии паспорта
+    public String patronymic; // поле для отчества
+    public String phone; //поле для телефона
+    public String surname; // поля для фамилии
+    public boolean subscription; //поле для подтвержения согласия новостной рассылки в СМС
+}
